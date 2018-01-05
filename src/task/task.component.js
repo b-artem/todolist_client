250163a2dc4c91fd48dcbe820037daa67e86dedc
b-artem <artemb.dev@gmail.com>
2018-01-05 17:13:59 +0200

    add edit task

--- src/task/task.component.js
+++ src/task/task.component.js
@@ -20,46 +20,46 @@ class TaskCtrl {
   constructor(Task, $uibModal) {
     this.taskService = Task;
     this.$uibModal = $uibModal;
-    // this.state = {
-    //   open: false,
-    //   edit: false
-    // }
+    this.state = {
+      // open: false,
+      edit: false
+    }
   }
 
   $onInit() {
     this.task = new this.taskService(this.task);
+    this.initialName = this.task.name;
   }
   //
   // toggleOpen() {
   //   this.state.open = !this.state.open;
   // }
   //
-  // toggleEdit() {
-  //   this.state.edit = !this.state.edit;
-  // }
-  //
-  // submitEdit() {
-  //   var self = this;
-  //   this.project.$update(function() {
-  //     self.initialName = self.project.name;
-  //     self.resetForm();
-  //     console.log('new name saved');
-  //   }, function(response) {
-  //     self.project.name = self.initialName;
-  //     console.log(response.data.error);
-  //   });
-  // }
-  //
-  // resetForm() {
-  //   this.editProjectForm.$setPristine();
-  //   this.state.edit = false;
-  // }
-  //
-  // cancelEdit() {
-  //   this.project.name = this.initialName;
-  //   this.resetForm();
-  // }
-  //
+  toggleEdit() {
+    this.state.edit = !this.state.edit;
+  }
+
+  submitEdit() {
+    var self = this;
+    this.task.$update(function() {
+      self.initialName = self.task.name;
+      self.resetForm();
+    }, function(response) {
+      self.task.name = self.initialName;
+      console.log(response.data.error);
+    });
+  }
+
+  resetForm() {
+    this.editTaskForm.$setPristine();
+    this.state.edit = false;
+  }
+
+  cancelEdit() {
+    this.task.name = this.initialName;
+    this.resetForm();
+  }
+
   delete() {
     var self = this;
     this.deleteModal().result.then(function() {

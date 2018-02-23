5636f9781bce36bfe0a55724f401627be436deec
b-artem <artemb.dev@gmail.com>
2018-02-23 22:25:30 +0200

    finish testing project and task components, start services

---
+++ src/project/project.component.spec.js
@@ -0,0 +1,81 @@
+'use strict';
+
+import project from './project.module';
+
+describe('Component project', function() {
+  var bindings;
+  var ctrl;
+  var onDeleteSpy = jasmine.createSpy('onDelete');
+
+  beforeEach(angular.mock.module(project));
+  beforeEach(inject(function($componentController) {
+    bindings = {
+      project: { name: 'Initial project', tasks: [] },
+      onDelete: onDeleteSpy
+    };
+    ctrl = $componentController('project', null, bindings);
+  }));
+
+  describe('ProjectCtrl', function() {
+    it('sets bindings properly', function() {
+      expect(ctrl.project).toEqual(bindings.project);
+      expect(ctrl.onDelete).toEqual(onDeleteSpy);
+    });
+
+    it('sets default values properly', function() {
+      expect(ctrl.errors).toEqual([]);
+      expect(ctrl.state.open).toBe(false);
+      expect(ctrl.state.edit).toBe(false);
+      expect(ctrl.state.allDone).toBe(false);
+    });
+
+    it('sets `initialName` variable in $onInit', function() {
+      ctrl.$onInit();
+      expect(ctrl.initialName).toEqual(bindings.project.name);
+    });
+
+    it('calls `checkAllDone` in $onInit', function() {
+      spyOn(ctrl, 'checkAllDone');
+      expect(ctrl.checkAllDone.calls.count()).toEqual(0);
+      ctrl.$onInit();
+      expect(ctrl.checkAllDone.calls.count()).toEqual(1);
+      expect(ctrl.checkAllDone.calls.argsFor(0)).toEqual([bindings.project.tasks]);
+    });
+
+    it('toggles `state.open`', function() {
+      expect(ctrl.state.open).toBe(false);
+      ctrl.toggleOpen();
+      expect(ctrl.state.open).toBe(true);
+      ctrl.toggleOpen();
+      expect(ctrl.state.open).toBe(false);
+    });
+
+    it('toggles `state.edit`', function() {
+      expect(ctrl.state.edit).toBe(false);
+      ctrl.toggleEdit();
+      expect(ctrl.state.edit).toBe(true);
+      ctrl.toggleEdit();
+      expect(ctrl.state.edit).toBe(false);
+    });
+
+    it('resets the form', function() {
+      var editProjectForm = jasmine.createSpyObj('editProjectForm', ['$setPristine']);
+      ctrl.editProjectForm = editProjectForm;
+      ctrl.errors = ['some error'];
+      ctrl.state.edit = true;
+      ctrl.resetForm();
+      expect(ctrl.errors).toEqual([]);
+      expect(ctrl.state.edit).toBe(false);
+    });
+
+    it('cancels `edit` state', function() {
+      var editProjectForm = jasmine.createSpyObj('editProjectForm', ['$setPristine']);
+      ctrl.editProjectForm = editProjectForm;
+      ctrl.project.name = 'Edited name';
+      spyOn(ctrl, 'resetForm');
+      ctrl.cancelEdit();
+      expect(ctrl.project.name).toEqual(bindings.project.name);
+      expect(ctrl.resetForm.calls.count()).toEqual(1);
+    });
+  });
+});
---
+++ src/task/task.component.spec.js
@@ -0,0 +1,88 @@
+'use strict';
+
+import task from './task.module';
+
+describe('Component project', function() {
+  var bindings;
+  var ctrl;
+  var onDeleteSpy = jasmine.createSpy('onDelete');
+  var onToggleDoneSpy = jasmine.createSpy('onToggleDone');
+  var onChangePrioritySpy = jasmine.createSpy('onChangePriority');
+
+  beforeEach(angular.mock.module(task));
+  beforeEach(inject(function($componentController) {
+    bindings = {
+      task: { name: 'Initial task' },
+      onDelete: onDeleteSpy,
+      onToggleDone: onToggleDoneSpy,
+      onChangePriority: onChangePrioritySpy
+    };
+    ctrl = $componentController('task', null, bindings);
+  }));
+
+
+  describe('TaskCtrl', function() {
+    it('sets bindings properly', function() {
+      expect(ctrl.task).toEqual(bindings.task);
+      expect(ctrl.onDelete).toEqual(onDeleteSpy);
+      expect(ctrl.onToggleDone).toEqual(onToggleDoneSpy);
+      expect(ctrl.onChangePriority).toEqual(onChangePrioritySpy);
+    });
+
+    it('sets default values properly', function() {
+      expect(ctrl.errors).toEqual([]);
+      expect(ctrl.state.edit).toBe(false);
+      expect(ctrl.state.urgent).toBe(false);
+    });
+
+    it('sets `initialName` variable in $onInit', function() {
+      ctrl.$onInit();
+      expect(ctrl.initialName).toEqual(bindings.task.name);
+    });
+
+    it('calls `setUrgency()` in $onInit', function() {
+      spyOn(ctrl, 'setUrgency');
+      expect(ctrl.setUrgency.calls.count()).toEqual(0);
+      ctrl.$onInit();
+      expect(ctrl.setUrgency.calls.count()).toEqual(1);
+    });
+
+    it('toggles `state.edit`', function() {
+      expect(ctrl.state.edit).toBe(false);
+      ctrl.toggleEdit();
+      expect(ctrl.state.edit).toBe(true);
+      ctrl.toggleEdit();
+      expect(ctrl.state.edit).toBe(false);
+    });
+
+    it('resets the form', function() {
+      var editTaskForm = jasmine.createSpyObj('editTaskForm', ['$setPristine']);
+      ctrl.editTaskForm = editTaskForm;
+      ctrl.errors = ['some error'];
+      ctrl.state.edit = true;
+      ctrl.resetForm();
+      expect(ctrl.errors).toEqual([]);
+      expect(ctrl.state.edit).toBe(false);
+    });
+
+    it('cancels `edit` state', function() {
+      var editTaskForm = jasmine.createSpyObj('editTaskForm', ['$setPristine']);
+      ctrl.editTaskForm = editTaskForm;
+      ctrl.task.name = 'Edited name';
+      spyOn(ctrl, 'resetForm');
+      expect(ctrl.resetForm.calls.count()).toEqual(0);
+      ctrl.cancelEdit();
+      expect(ctrl.task.name).toEqual(bindings.task.name);
+      expect(ctrl.resetForm.calls.count()).toEqual(1);
+    });
+
+    it('sets urgency', function() {
+      ctrl.task.deadline = '2018-01-25T10:00:23.021Z';
+      ctrl.setUrgency();
+      expect(ctrl.state.urgent).toBe(true);
+      ctrl.task.deadline = '2038-01-25T10:00:23.021Z';
+      ctrl.setUrgency();
+      expect(ctrl.state.urgent).toBe(false);
+    });
+  });
+});
